196b5af0ff4d7831fe732e8df2d39c8284aaf85a
Avi Jassra <avijassra@hotmail.com>
2014-01-28 12:46:28 -0500

    sample code

---
+++ Todos/app/js/app-controller.js
@@ -0,0 +1,29 @@
+﻿var keeperApp = angular.module('TodoApp', ['ngRoute']);
+
+keeperApp.service('TodoSrvc', function () {
+    return {
+        get: function () {
+            return [{
+                title: 'F#',
+                desc: 'learn f#'
+            }, {
+                title: 'lib',
+                desc: 'return books'
+            }];
+        }
+    };
+});
+
+keeperApp.controller('TodoCtrl', ['$scope', 'TodoSrvc', function ($scope, TodoSrvc) {
+    $scope.searchBy = null;
+    $scope.isDisabled = !$scope.searchBy;
+    $scope.notes = TodoSrvc.get();
+
+    $scope.onSearch = function () {
+        // to be implemented
+    };
+
+    $scope.$watch('searchBy', function () {
+        $scope.isDisabled = !$scope.searchBy;
+    });
+}]);
